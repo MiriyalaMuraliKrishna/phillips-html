875e2d8c16439653198f762d734c20b90bfe2321
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2022-12-27 13:57:38 +0530

    dec27 changes

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ var $ = jQuery.noConflict();
 $(document).ready(function(){
     $(window).on('scroll', function(){
         var scroll = $(this).scrollTop();
-        if(scroll > 4){
-            $('.main-header').addClass('fixed_header');
+        if(scroll > 5){
+            $('.main-header').addClass('fixed_header').removeClass('sticky_header');
         }
         else{
-            $('.main-header').removeClass('fixed_header');
+            $('.main-header').removeClass('fixed_header').addClass('sticky_header');
         }
     });
+    let reverseHeader = $('.home');
+    if(reverseHeader !='undefined' && reverseHeader.length > 0){
+        $('.main-header').addClass('sticky_header');
+        $('.site-main-cover').addClass('no_space');
+    }
     
 });
