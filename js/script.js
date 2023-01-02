3801f74589af50588639403c7bae66b6a3354e19
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-01-02 18:39:44 +0530

    added pages

--- js/script.js
+++ js/script.js
@@ -3,16 +3,15 @@ $(document).ready(function(){
     $(window).on('scroll load', function(){
         var scroll = $(this).scrollTop();
         if(scroll > 5){
-            $('.main-header').addClass('fixed_header').removeClass('sticky_header');
+            $('.main-header').addClass('fixed_header');
         }
         else{
-            $('.main-header').removeClass('fixed_header').addClass('sticky_header');
+            $('.main-header').removeClass('fixed_header');
         }
     });
     let reverseHeader = $('.home');
     if(reverseHeader !='undefined' && reverseHeader.length > 0){
         $('.main-header').addClass('sticky_header');
         $('.site-main-cover').addClass('no_space');
-    }
-    
+    }    
 });
