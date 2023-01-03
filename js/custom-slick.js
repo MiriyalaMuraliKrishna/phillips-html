e55f0cf1f523ed6cdfcdc613f46b9f6c92d7d2b0
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-01-03 15:24:40 +0530

    slider addded arrwows news

--- js/custom-slick.js
+++ js/custom-slick.js
@@ -19,4 +19,33 @@ $(document).ready(function() {
         }
     ]
    });
+  const $dotsContainer = $(".our-office-slider ul.slick-dots");
+   if (typeof $dotsContainer !== "undefined" && $dotsContainer.length > 0){
+      $dotsContainer.addClass('office-slick-dots');
+   }
+
+          $('.news-event-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+             vertical: false,
+             autoplay: false,
+          }
+        }
+    ]
+   });
+
+            const $newsContainer = $(".news-event-slider ul.slick-dots");
+   if (typeof $newsContainer !== "undefined" && $newsContainer.length > 0){
+      $newsContainer.addClass('news-slick-dots');
+   }
+
 });
