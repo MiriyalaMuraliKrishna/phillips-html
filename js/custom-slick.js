6fee10bc1af62d1f4d17720ac4082eacec4d6622
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-01-03 12:49:31 +0530

    slider addded arrwows

--- js/custom-slick.js
+++ js/custom-slick.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       slidesToScroll: 1,
       dots: true,
       arrows: false,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 3000,
       variableWidth: true,
       responsive: [
