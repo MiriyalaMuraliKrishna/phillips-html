86f7b5ab3fe491f7252df7b430184846bbe4b02a
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-02-08 11:21:32 +0530

    js moved to slick

--- js/custom-slick.js
+++ js/custom-slick.js
@@ -88,4 +88,78 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
     });
     
+
+
+
+    $('.community-event-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: '<span class="timeline-arrow prev-arrow fa-light fa-angle-left flex flex-center"></span>',
+        nextArrow: '<span class="timeline-arrow next-arrow fa-light fa-angle-right flex flex-center"></span>',
+        autoplay: false,
+        autoplaySpeed: 3000,
+        variableWidth: true,
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    vertical: false,
+                    autoplay: false,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                    settings: {
+                        vertical: false,
+                        autoplay: false,
+                        arrows: false,
+                    }
+                }
+        ]
+    });
+    
+    const $communityContainer = $(".community-event-slider ul.slick-dots");
+        if (typeof $communityContainer !== "undefined" && $communityContainer.length > 0){
+            $communityContainer.addClass('news-slick-dots');
+        }
+    
+    $('.careers-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: '<span class="timeline-arrow prev-arrow fa-light fa-angle-left flex flex-center"></span>',
+        nextArrow: '<span class="timeline-arrow next-arrow fa-light fa-angle-right flex flex-center"></span>',
+        autoplay: false,
+        autoplaySpeed: 3000,
+        variableWidth: true,
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    vertical: false,
+                    autoplay: false,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                    settings: {
+                        vertical: false,
+                        autoplay: false,
+                        arrows: false,
+                    }
+                }
+        ]
+    });
+    const $presentersContainer1 = $(".careers-slider ul.slick-dots");
+        if (typeof $presentersContainer1 !== "undefined" && $presentersContainer1.length > 0){
+            $presentersContainer1.addClass('news-slick-dots');
+        }
+    
+    
+
 });
