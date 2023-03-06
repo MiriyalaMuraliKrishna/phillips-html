ef84add998e2f1c22e59e1da32b77f2f0a07f921
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2023-03-06 13:32:55 +0530

    05 started

--- js/script.js
+++ js/script.js
@@ -180,23 +180,6 @@ let mobileScreen = function(){
 $(window).resize(function () { mobileScreen(); });
 $(document).on('ready', function () { mobileScreen(); });
 
-
-$(window).on("load", function(){
-    let headerHeight = $('.main-header').outerHeight(true);
-    $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top - headerHeight
-                }, 1000);
-                return false;
-            }
-        }
-    });
-});
-
 $(document).ready(function(){
     if($(".bio-people-main").length > 0 ){
         $(window).on("scroll load", function(){
